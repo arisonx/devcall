e43176fb13a7fbd3268b0bc1561580dd60be3aaa
arisonreis <arisontechp@gmail.com>
2023-12-31 06:17:19 -0300

    update create user API Route

--- client/src/app/api/user/create/route.ts
+++ client/src/app/api/user/create/route.ts
@@ -1,37 +1,37 @@
-import { prisma } from "../../../../lib/prisma/prisma";
-import { UserData } from "./interface";
-import { sign } from "jsonwebtoken";
-import { cookies } from "next/headers";
+import { prisma } from '../../../../lib/prisma/prisma';
+import { UserData } from './interface';
+import { sign } from 'jsonwebtoken';
+import { cookies } from 'next/headers';
 
 export async function POST(request: Request) {
-  const { name }: UserData = await request.json();
+ const { name }: UserData = await request.json();
 
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        name,
-      },
-    });
+ try {
+  const newUser = await prisma.user.create({
+   data: {
+    name,
+   },
+  });
 
-    const secret = process.env.JWT_SECRET as string;
-    const token = sign({ id: newUser.id }, secret, {
-      expiresIn: "7d",
-    });
+  const secret = process.env.JWT_SECRET as string;
+  const token = sign({ id: newUser.id }, secret, {
+   expiresIn: '7d',
+  });
 
-    cookies().set({
-      name: "devcall:auth",
-      value: token,
-      httpOnly: true,
-      path: "/",
-      maxAge: 60 * 60 * 24 * 7 // 2 days,
-    });
+  cookies().set({
+   name: 'devcall:auth',
+   value: token,
+   httpOnly: true,
+   path: '/',
+   maxAge: 60 * 60 * 24 * 7, // 2 days,
+  });
 
-    return new Response(token, {
-      status: 201,
-    });
-  } catch (err) {
-    return new Response("Internal server Error", {
-      status: 500,
-    });
-  }
+  return new Response(token, {
+   status: 201,
+  });
+ } catch (err) {
+  return new Response('Internal server Error', {
+   status: 500,
+  });
+ }
 }
