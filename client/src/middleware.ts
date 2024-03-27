b11300e301fbdbc17e6bd7173e77b602f0c19c43
arisonreis <arisontechp@gmail.com>
2024-03-27 20:50:31 -0300

    update middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,9 +1,5 @@
 import { NextRequest } from 'next/server';
-
-import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { delete_cookie } from './app/actions/deleteCookie';
-import { authOptions } from './app/lib/auth';
 
 export async function middleware(request: NextRequest) {
  const cookies = request.cookies;
