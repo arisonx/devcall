fb6e9f1f76e2b963264fc468f49082e700559956
voidex1 <arisontechp@gmail.com>
2024-05-01 23:53:01 -0300

    add broadcast messsage emitter

--- server/src/socket/socket.service.ts
+++ server/src/socket/socket.service.ts
@@ -3,18 +3,21 @@ import { Socket } from 'socket.io';
 
 @Injectable()
 export class SocketService {
-  private readonly connectedClients: Map<string, Socket> = new Map();
+  private readonly connectedClients: Map<string, boolean> = new Map();
 
   handle_connection(socket: Socket): void {
     const client_id = socket.id;
 
-    this.connectedClients.set(client_id, socket);
+    this.connectedClients.set(client_id, socket.connected);
 
-    console.log(client_id);
+    console.log(this.connectedClients);
   }
 
-
   handleMessageBroadcast(socket: Socket, payload: string): void {
+    socket.emit('broadcast', {
+      from: socket.id,
+      payload: payload
+    });
     console.log(payload);
   }
 }
