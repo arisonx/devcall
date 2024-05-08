bd2e78c6d6624a576baff44e1addd471f503400e
voidex1 <arisontechp@gmail.com>
2024-05-08 04:49:07 -0300

    add chatroom

--- server/src/socket/socket.service.ts
+++ server/src/socket/socket.service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { Socket } from 'socket.io';
-
+import { Server, Socket } from 'socket.io';
 
 @Injectable()
 export class SocketService {
   private readonly connectedClients: Map<string, boolean> = new Map();
 
-  handle_connection(socket: Socket): void {
+  handle_connection(socket: Socket, serverSocket: Server): void {
     const client_id = socket.id;
-
     this.connectedClients.set(client_id, socket.connected);
 
-    console.log(this.connectedClients);
+    socket.join('chatroom');
+
+    const roomclients = serverSocket.sockets.adapter.rooms.get('chatroom');
+
+    console.log(roomclients);
   }
 
   handleMessageBroadcast(socket: Socket, payload: string) {
-    console.log(payload)
-    socket.emit('broadcast', {
+    console.log(payload);
+
+    socket.to('chatroom').emit('message', {
       from: socket.id,
       payload: payload,
     });
+  }
+
+  handleDisconectedClients(socket: Socket) {
+    console.log('disconected client');
+    socket.disconnect(true);
+  }
+
+  handleTyping(socket: Socket, username: string) {
+    socket.to('chatroom').emit('typing', username);
   }
 }
