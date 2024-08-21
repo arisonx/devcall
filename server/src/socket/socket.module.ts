be4e4303197ae876da19de3bffdbed06cb8c6b41
voidex1 <arisontechp@gmail.com>
2024-08-21 02:21:55 -0300

    removing files

--- server/src/socket/socket.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SocketService } from './socket.service';
-import {SocketGateway} from './socket.gateway'
-
-@Module({
-  providers: [SocketService, SocketGateway],
-})
-export class SocketModule {}
